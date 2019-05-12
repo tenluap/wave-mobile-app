b6506a046d89d99b9b607ee591af103144f04aa6
ehigiepaul <ehigiepaul@yahoo.com>
2019-05-12 18:16:24 +0100

    DONE: comic view design completed

--- app/views/comic/comic-view-model.ts
+++ app/views/comic/comic-view-model.ts
@@ -1,5 +1,6 @@
-import { Observable } from 'tns-core-modules/data/observable';
+import { Observable, EventData } from 'tns-core-modules/data/observable';
 import { screen } from 'tns-core-modules/platform/platform';
+import { Page } from 'tns-core-modules/ui/frame/frame';
 
 export class ComicViewModel extends Observable {
     width;
@@ -7,6 +8,23 @@ export class ComicViewModel extends Observable {
     constructor() {
         super();
 this.width = (screen.mainScreen.widthDIPs / 2) -30
+    }
+  
+    goto(ev: EventData) {
+        let selected = <Page>ev.object
+        var page = selected.page.frame.parent.parent.page.frame
+      
+
+        page.navigate({
+            moduleName: "views/comic/comic-view-page",
+            transition: {
+                name: "slide",
+                // duration: 300
+            }
+        })
+       
+
+        // console.log(selected)
     }
 }
     
